69327b83fd6084269cdb6ab8d36ed1813e22a54a
Dhruv Lawaniya <73819794+DhruvLawaniya@users.noreply.github.com>
2024-08-11 20:02:01 +0530

    hello

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-
+import moengage from "@moengage/web-sdk";
 import LoginForm from './LoginForm';
 import LandingPage from './LandingPage';
 import SignInLandingPage from './SignInLandingPage';
 
+
 function App() {
+  moengage.initialize({app_id: '5DWH5EWA6O661HFBFC5SUCCO'});
   return (
     <div className="App">
       <Routes>
